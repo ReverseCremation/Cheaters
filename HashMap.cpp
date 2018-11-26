139c336a947ba281e85cf6ec1816c349fe280684
ReverseCremation <jplata@utexas.edu>
2018-11-26 13:08:37 -0600

    added HashMap class; No prototypes or implementations actually written yet

---
+++ HashMap.cpp
@@ -0,0 +1,4 @@
+//
+// Created by jeronimo on 11/26/18.
+//
+
---
+++ HashMap.h
@@ -0,0 +1,18 @@
+//
+// Created by jeronimo on 11/26/18.
+//
+
+#ifndef CHEATERS_HASHTABLE_H
+#define CHEATERS_HASHTABLE_H
+
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif //CHEATERS_HASHTABLE_H
